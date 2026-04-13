0fb33826f508c0d74b65e1b1248fec672618ee4f
barcius <denis.gorbunov@vigram.com>
2026-04-13 15:55:58 +0300

    removed trash

--- src/widgets/messagePane/index.tsx
+++ src/widgets/messagePane/index.tsx
@@ -8,8 +8,6 @@ const MessagePane: React.FC<{ message: Message; }> = ({ message }) => {
   const setEditMessageId = useStore((state) => state.setEditMessageId);
   const editMessage = useStore((state) => state.editMessage);
 
-  const editInputRef = useRef<HTMLInputElement>(null);
-
   const createdAt = new Date(message.timeStamp).toLocaleString();
 
   const handleContextMenu = (e: React.MouseEvent) => {
@@ -22,8 +20,8 @@ const MessagePane: React.FC<{ message: Message; }> = ({ message }) => {
   };
 
   const handleEditMessage = (val: string) => {
-      editMessage(message.id, val);
-      setEditMessageId(null);
+    editMessage(message.id, val);
+    setEditMessageId(null);
   };
 
   const isEditing = editMessageId === message.id;
@@ -39,17 +37,6 @@ const MessagePane: React.FC<{ message: Message; }> = ({ message }) => {
           defaultInputValue={message.text}
           onButtonClick={handleEditMessage}
         />
-        // <div className='flex'>
-        //   <input
-        //     className='flex-1 mr-2 p-2'
-        //     type='text'
-        //     ref={editInputRef}
-        //     placeholder='Type a message'
-        //     defaultValue={message.text}
-        //     autoFocus
-        //   />
-        //   <button onClick={handleEditMessage}>Save</button>
-        // </div>
       ) : (
         <div>{message.text}</div>
       )}
